a96e7d6bde9a9e4b2618c15489bd5dbf42d73342
huot32@gmail.com <Omega!23>
2015-05-31 01:17:57 -0400

    Initial Install
    Signed-off-by: huot32@gmail.com <Omega!23>

---
+++ Assets/Scripts/GameObjects/GolfTerrain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace GolfGPS.Scripts
+{
+    public class GolfTerrain : MonoBehaviour
+    {
+        private Terrain _ter;
+        private TerrainData _td;
+
+        public void Awake()
+        {
+            _ter = GetComponent<Terrain>();
+            _td = _ter.terrainData;
+            _td.size = new Vector3(100, 1, 400);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Models/Golf/Course.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace GolfGPS.Models
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public float Slope { get; set; }
+        public float Rating { get; set; }
+
+        public List<Hole> Holes { get; set; }
+    }
+}
